7a9e1b3263edde85acac27f692d7e12fda77c86a
lambovg <georgi.lambov@gmail.com>
2022-08-24 12:31:38 +0300

    Initial and refactor with Struct.

---
+++ request.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Response struct {
+	body string
+	err  error
+}
+
+type Get struct {
+	url string
+}
+
+type Post struct {
+	Get
+}
+
+func (r Response) Response() {
+	log.Printf(r.body)
+
+	if r.err != nil {
+		log.Fatalln(r.err)
+	}
+}
+
+func (r Get) Request() {
+	resp, err := http.Get(r.url)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	var response = Response{body: string(body), err: err}
+	response.Response()
+}
+
+func (r Post) Request() {
+	//TODO implementation
+}
+
+func main() {
+	var request = Get{url: "https://d2kgi8nio2h9bn.cloudfront.net/hello-world.json"}
+	request.Request()
+}
+
+//func main() {
+//	resp, err := http.Get("https://d2kgi8nio2h9bn.cloudfront.net/hello-world.json")
+//
+//	if err != nil {
+//		log.Fatalln(err)
+//	}
+//
+//	body, err := ioutil.ReadAll(resp.Body)
+//
+//	var response = Response{body: string(body), err: err}
+//	response.Response()
+//
+//	//sb := string(body)
+//	//log.Printf(sb)
+//}
