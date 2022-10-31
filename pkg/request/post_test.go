77aed3e17c6247b0db396993fe43827d5f1df739
lambovg <georgi.lambov@gmail.com>
2022-10-31 20:31:21 +0200

    Post test with empty body.

--- pkg/request/post_test.go
+++ pkg/request/post_test.go
@@ -46,7 +46,7 @@ func TestGivenBuildParams_whenPost_thenReturnUrl(t *testing.T) {
 }
 
 func TestGivenBuildParams_whenPost_thenReturnRequestBody(t *testing.T) {
-	server := postServer(t)
+	server := getServer(t)
 	defer server.Close()
 
 	params := Params{Hostname: "localhost", Port: 80, Protocol: "http", Path: "/"}
@@ -58,7 +58,7 @@ func TestGivenBuildParams_whenPost_thenReturnRequestBody(t *testing.T) {
 }
 
 func TestGivenUrl_whenPost_thenReturnRequestBody(t *testing.T) {
-	server := postServer(t)
+	server := getServer(t)
 	defer server.Close()
 
 	future := Post(server.URL)
@@ -67,7 +67,7 @@ func TestGivenUrl_whenPost_thenReturnRequestBody(t *testing.T) {
 }
 
 func TestGivenClient_whenPost_thenReturnRequestBody(t *testing.T) {
-	server := postServer(t)
+	server := getServer(t)
 	defer server.Close()
 
 	client := &http.Client{Timeout: 30 * time.Second}
