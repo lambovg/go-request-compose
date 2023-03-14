505a44acebd5414ea80d2d984b0e02af1fd32394
lambovg <georgi.lambov@gmail.com>
2023-03-14 16:51:51 +0200

    Head request by url.

--- pkg/request/head_test.go
+++ pkg/request/head_test.go
@@ -20,6 +20,18 @@ func TestGivenParams_whenHead_thenReturnRequestBody(t *testing.T) {
 	test.Ok(t, future().Header.Get("Content-Length"), fmt.Sprintf("%v", 20))
 }
 
+func TestGivenUrl_whenHead_thenReturnRequestBody(t *testing.T) {
+	server := headServer(t)
+	defer server.Close()
+
+	future := Head(server.URL)
+
+	test.Ok(t, future().Body, "")
+	test.Equals(t, future().StatusCode, 200)
+	test.Ok(t, future().Status, "200 OK")
+	test.Ok(t, future().Header.Get("Content-Length"), fmt.Sprintf("%v", 20))
+}
+
 func headServer(t *testing.T) *httptest.Server {
 	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 		rw.Header().Set("Content-Length", "20")
